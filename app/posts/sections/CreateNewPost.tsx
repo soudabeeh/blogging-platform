a3aa84ed9d85619985cfdda57381f98fd357684b
soudabe <soudabe@soudabes-MacBook-Pro.local>
2023-10-28 03:51:49 +0330

    add yup validation

--- app/posts/sections/CreateNewPost.tsx
+++ app/posts/sections/CreateNewPost.tsx
@@ -10,7 +10,22 @@ import createSinglePost from "@/app/actions/createSinglePost";
 import TextArea from "@/components/ui/TextArea";
 import { toast } from "react-toastify";
 import Toast from "@/components/ui/Toast";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 const DynamicButton = dynamic(() => import("@/components/ui/Button/Button"));
+
+type TPostForm = {
+  title: string;
+  body: string;
+  userId: string;
+};
+
+const ProfileFormVS: Yup.ObjectSchema<TPostForm> = Yup.object().shape({
+  title: Yup.string().required("لطفا عنوان را وارد کنید"),
+  body: Yup.string().required("لطفا توضیحات را وارد کنید"),
+  userId: Yup.string().required("لطفا آیدی کاربر را وارد کنید"),
+});
 
 const CreateNewPost = () => {
   const {
@@ -18,8 +33,9 @@ const CreateNewPost = () => {
     register,
     handleSubmit,
     reset,
+    clearErrors,
     formState: { errors, isSubmitting, isDirty, isValid },
-  } = useForm<any>({});
+  } = useForm<TPostForm>({ resolver: yupResolver(ProfileFormVS) });
 
   const { modalProps: createModalProps, handelOpen: showCreatePostModal } =
     useModal();
@@ -29,7 +45,6 @@ const CreateNewPost = () => {
     if (res?.status === 201 || res?.status === 200) {
       createModalProps.onClose();
       revalidatePosts();
-      resetFormState();
       toast("پست جدید با موفقیت ایجاد شد.");
     }
   };
@@ -45,7 +60,15 @@ const CreateNewPost = () => {
   return (
     <div className='flex flex-col'>
       <div>
-        <DynamicButton onClick={showCreatePostModal}>افزودن پست</DynamicButton>
+        <DynamicButton
+          onClick={() => {
+            showCreatePostModal();
+            clearErrors(["title", "body", "userId"]);
+            resetFormState();
+          }}
+        >
+          افزودن پست
+        </DynamicButton>
         <Modal {...createModalProps} title='ایجاد پست'>
           <div className='w-[300px] xl:w-[900px]'>
             <form
@@ -58,17 +81,20 @@ const CreateNewPost = () => {
                     {...register("title")}
                     label='عنوان'
                     className='w-full'
+                    errorMessage={errors?.title?.message}
                   />
                   <Input
                     {...register("userId")}
                     label='ایدی کاربر'
                     className='w-full'
+                    errorMessage={errors?.userId?.message}
                   />
                 </div>
                 <TextArea
                   {...register("body")}
                   className='w-full'
                   label='توضیحات'
+                  errorMessage={errors?.body?.message}
                 />
               </div>
               <div className='flex justify-end pt-4 px-4 '>
